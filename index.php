b4a5bddffe2a7e08772a6bb4914ed8d949b07a0c
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2021-10-31 01:08:50 +0100

    commit 2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <meta name="author" content="harrnish">
 
-      <title>Daniel Avery</title>
+      <title>Daniel Avery Videographic</title>
 
       <!-- stylesheet -->
       <link rel="stylesheet" href="style.css">
